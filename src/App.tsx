783c8f2e2b0a872dae0ec78d0d90f702f22f4ebe
flgisimone <florestagiuliosimone@gmail.com>
2023-05-03 13:16:44 +0200

    add searchbar with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import Searchbar from './components/Searchbar/Searchbar';
+import './App.css';
 
 function App() {
-
   return (
     <main>
-      
+      <Searchbar />
     </main>
   )
 }
