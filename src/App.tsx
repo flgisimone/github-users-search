79ae4a4b1c9d7f660e18b3bb387d3f9ecbca0bb4
flgisimone <florestagiuliosimone@gmail.com>
2023-05-11 11:35:54 +0200

    fixed various

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { AppProvider } from "./utils/context.tsx"
 import Searchbar from './components/Searchbar/Searchbar';
 import UserTab from "./components/UserTab/UserTab.tsx";
 import Footer from "./components/Footer/Footer.tsx";
-
 import './App.css';
 
 function App() {
